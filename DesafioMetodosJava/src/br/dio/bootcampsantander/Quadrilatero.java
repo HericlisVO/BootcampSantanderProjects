bdb72c9dd5dfa95149576c2f94ad8d588c3d0724
Hericlis Ventura <hericlisventura.o@gmail.com>
2022-07-31 14:03:47 -0300

    aprendendo mais sobre operadores relacionais

--- DesafioMetodosJava/src/br/dio/bootcampsantander/Quadrilatero.java
+++ DesafioMetodosJava/src/br/dio/bootcampsantander/Quadrilatero.java
@@ -2,24 +2,27 @@ package br.dio.bootcampsantander;
 
 public class Quadrilatero {
 
+    public static double area(double lado) {
 
-    public static void area(double lado) {
-
-        System.out.println("Área do quadrado:" + lado * lado);
+        return lado * lado;
     }
 
-    public static void area(double lado1, double lado2) {
+    public static double area(double lado1, double lado2) {
 
-        System.out.println("Área do retângulo:" + lado1 * lado2);
+        return lado1 * lado2;
     }
 
-    public static void area(double baseMaior, double baseMenor, double altura) {
+    public static double area(double baseMaior, double baseMenor, double altura) {
 
-        System.out.println("Área do trapézio:" + ((baseMaior+baseMenor)*altura) / 2);
+        return ((baseMaior+baseMenor)*altura) / 2;
     }
 
-    public static void area(float diagonal1, float diagonal2) {
+    public static void xpto() {
 
-        System.out.println("Área do losango:" + (diagonal1 * diagonal2)/2);
+        System.out.println("Antes");
+        return;
     }
-}
+
+    public static double abc() {
+        return 1.6;
+    }}
